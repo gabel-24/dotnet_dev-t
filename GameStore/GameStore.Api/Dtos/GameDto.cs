0197a52dd2a73637c0ba91efa7e700a60a4665dc
gabel-24 <gabeldash828@gmail.com>
2026-07-23 12:35:03 +0300

    gamestore api project

---
+++ GameStore/GameStore.Api/Dtos/GameDto.cs
@@ -0,0 +1,14 @@
+namespace GameStore.Api.Dtos;
+
+//A DTO (Data Transfer Object) is a simple object that is used to transfer data between layers of an application. 
+// In this case, the GameDto class is likely used to transfer data related to a game in the GameStore application. 
+// The record class syntax in C# is used to define immutable data objects with value-based equality.
+public record GameDto
+(
+    int Id,
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
+  
